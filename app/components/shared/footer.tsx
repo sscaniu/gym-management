5e3ff2248d7e4ed5a79e4c492580a9f7c273902f
sscaniu <sergcaniu@gmail.com>
2024-02-08 15:56:21 -0500

    Fixed footer to bottom of the window. Content flows under, which may not be the desired outcome. To configure that the footer flows with the content removed 'fixed' from the classes

--- app/components/shared/footer.tsx
+++ app/components/shared/footer.tsx
@@ -2,7 +2,7 @@ import { jost } from "./font";
 
 export default function Footer() {
   return (
-    <footer className={`${jost.className} absolute inset-x-0 bottom-0 right-0 z-50 flex h-24 text-xs font-medium leading-[relaxed] tracking-[0px] text-white`} >
+    <footer className={`${jost.className} fixed bottom-0 left-0 right-0 inset-x-0  z-50 flex h-24 text-xs font-medium leading-[relaxed] tracking-[0px] text-white`} >
       <div className="flex flex-grow flex-col items-center justify-between self-stretch bg-slate-900 px-6 pb-8 pt-6" >
         <div className="flex items-center">
           <span className="text-center">
