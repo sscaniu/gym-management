cfbf48b8c8d21ae961e4d0ac4e3568b19ab0a359
sscaniu <sergcaniu@gmail.com>
2024-02-08 11:55:27 -0500

    Moved the google fonts to font components and updated the header and footer

---
+++ app/components/shared/footer.tsx
@@ -0,0 +1,17 @@
+import { jost } from "./font";
+
+export default function Footer() {
+  return (
+    <footer className={`${jost.className} absolute inset-x-0 bottom-0 right-0 z-50 flex h-24 text-xs font-medium leading-[relaxed] tracking-[0px] text-white`} >
+      <div className="flex flex-grow flex-col items-center justify-between self-stretch bg-slate-900 px-6 pb-8 pt-6" >
+        <div className="flex items-center">
+          <span className="text-center">
+            {"©2021 GymBuddies, LLC. | All Rights Reserved. | Privacy Policy"}
+            <br />
+            {'Version 2024.1'}
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+}
