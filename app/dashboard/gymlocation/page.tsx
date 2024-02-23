90d16c8da30903edd41db9444f6357bc5b519325
sscaniu <sergcaniu@gmail.com>
2024-02-22 19:07:54 -0500

    Added and styled a bitcards. Background is still giving me issues

--- app/dashboard/gymlocation/page.tsx
+++ app/dashboard/gymlocation/page.tsx
@@ -1,9 +1,25 @@
 /*Interface to add a new Gym to an account*/
 
+import InputNavHeader from "@/app/components/shared/InputNavHeader"
+
 export default function GymLocation() {
 
     return (
-        <div className="bg-vectorcurve w-full h-screen">test</div>
+
+        <article className="flex-1 z-10 bg-gradient-to-b from-oxford-blue from-50% to-delft-blue to-50%">
+
+            <div className="mx-[56px] my-10">
+
+                <InputNavHeader
+                    name={"Gym Location"}
+                    description={"Complete the fields below to add your gym profile"}
+                />
+                <div className="text-white">
+                    Stuff here
+                </div>
+            </div>
+
+        </article>
     )
 
 
