c678ba549b4639cf9b26313055b4aa93b9ab5a01
sscaniu <sergcaniu@gmail.com>
2024-02-24 16:56:08 -0500

    Moved the buttons to the top of the grid

--- app/components/shared/InputNavHeader.tsx
+++ app/components/shared/InputNavHeader.tsx
@@ -18,31 +18,30 @@ export default function InputNavHeader(
 
 
     return (
+        <>
 
+            <div className={`${rubik.className} text-white grid grid-cols-2 grid-rows-2 bg-card-blue-500`}>
+                <div className="text-2xl">{name}</div>
 
-        <div className={`${rubik.className} text-white grid grid-cols-2 grid-rows-2 bg-card-blue-500`}>
-            <div className="text-2xl">{name}</div>
-            <div></div>
-            <div className="text-base">{description}</div>
+                <div className="flex justify-end">
+                    <Button
+                        text={"Previous"}
+                        style={ButtonStyles.Primary}
+                        addCSS={"w-[100px] mr-5"}
+                    />
+                    <Button
+                        text={"Next"}
+                        style={ButtonStyles.Primary}
+                        addCSS={"w-[100px]"}
+                    />
 
-
-            <div className="flex justify-end">
-                <Button
-                    text={"Previous"}
-                    style={ButtonStyles.Primary}
-                    addCSS={"w-[100px] mr-5"}
-                />
-                <Button
-                    text={"Next"}
-                    style={ButtonStyles.Primary}
-                    addCSS={"w-[100px]"}
-                />
+                </div>
+                <div className="text-base">{description}</div>
+                <div></div>
 
             </div>
 
-        </div >
-
-
+        </>
 
     )
 
