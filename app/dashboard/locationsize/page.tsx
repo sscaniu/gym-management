ba609547257bb6bd3afbc1a98a7e0317c99110b5
sscaniu <sergcaniu@gmail.com>
2024-02-26 11:00:00 -0500

    Initial load of the gym location selection page

---
+++ app/dashboard/locationsize/page.tsx
@@ -0,0 +1,39 @@
+/*Interface to selection location size*/
+"use client";
+import { TypeSelections, TypeSelectionConfig } from "@/app/components/shared/TypeSelections";
+
+export default function LocationSize() {
+
+    const defaultConfig = [
+        {
+            id: "small",
+            name: "Small",
+            description: "Membership size between 1-20 clients",
+            iconPath: ""
+        },
+        {
+            id: "medium",
+            name: "Medium",
+            description: "Membership size between 21-60 clients",
+            iconPath: ""
+        },
+        {
+            id: "large",
+            name: "Large",
+            description: "Membership size between 61-100+ clients",
+            iconPath: ""
+        }
+    ]
+
+    return (
+        <article className="flex-1 z-10 bg-gradient-to-b from-oxford-blue from-50% to-delft-blue to-50%">
+            <TypeSelections
+                pageHeader={"Location Size"}
+                pageDescription={"Which of the following choices best describes your gym's membership size?"}
+                config={defaultConfig} />
+        </article>
+    );
+
+
+
+}
