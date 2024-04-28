749200f4a5cb1ce4eb0a218af92340e4346a05d4
sscaniu <sergcaniu@gmail.com>
2024-04-28 18:46:19 -0400

    Demo messaging interface

--- app/dashboard/messages/page.tsx
+++ app/dashboard/messages/page.tsx
@@ -1,227 +1,185 @@
 "use client";
+import { rubik } from "@/app/components/shared/font";
+import { sendMessage } from "@/app/lib/actions";
+import Image from 'next/image';
+import { useState } from 'react';
+import { ChatMessage, Message, ChatStyle } from '@/app/components/shared/ChatMessage'
 
-import { useState } from "react";
-import Image from "next/image";
-import { CheckCircleIcon } from "@heroicons/react/24/solid";
+interface Tab {
+  name: string,
+  href: string,
+  current: boolean
+}
 
-const invoice = {
-  subTotal: "$8,800.00",
-  tax: "$1,760.00",
-  total: "$10,560.00",
-  items: [
-    {
-      id: 1,
-      title: "Logo redesign",
-      description: "New logo and digital asset playbook.",
-      hours: "20.0",
-      rate: "$100.00",
-      price: "$2,000.00",
-    },
-    {
-      id: 2,
-      title: "Website redesign",
-      description: "Design and program new company website.",
-      hours: "52.0",
-      rate: "$100.00",
-      price: "$5,200.00",
-    },
-    {
-      id: 3,
-      title: "Business cards",
-      description: 'Design and production of 3.5" x 2.0" business cards.',
-      hours: "12.0",
-      rate: "$100.00",
-      price: "$1,200.00",
-    },
-    {
-      id: 4,
-      title: "T-shirt design",
-      description: "Three t-shirt design concepts.",
-      hours: "4.0",
-      rate: "$100.00",
-      price: "$400.00",
-    },
-  ],
-};
-const activity = [
-  {
-    id: 4,
-    type: "commented",
-    person: {
-      name: "Chelsea Hagon",
-      imageUrl: "/images/dashboard/client.png",
-    },
-    comment:
-      "Called client, they reassured me the invoice would be paid by the 25th.",
-    date: "3d ago",
-    dateTime: "2023-01-23T15:56",
-  },
-];
+const tabs: Tab[] = [
+  { name: 'Profile', href: '#', current: false },
+  { name: 'Goals', href: '#', current: false },
+  { name: 'Messages', href: '#', current: true },
+  { name: 'Notes', href: '#', current: false },
+  { name: 'Client Calender', href: '#', current: false },
+]
 
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
+
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
 }
 
 export default function Messages() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  //State for messages
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  //Wrapper that calls the sendMessage function and then updates the state of the UI
+  async function sendWithUpdate(formData: FormData) {
+
+    sendMessage(formData);
+
+    //Update the UI
+    var newMessage: Message = {
+      id: "1",
+      speaker: "SH",
+      message: String(formData.get('message')),
+      time: "8:00 PM",
+      style: ChatStyle.Trainer
+    }
+    var newMessages = [...messages];
+    newMessages.push(newMessage);
+
+    setMessages(newMessages);
+
+  }
 
   return (
+    // Header
     <>
-      <main>
-        <header className="relative isolate pt-16">
-          <div
-            className="absolute inset-0 -z-10 overflow-hidden"
-            aria-hidden="true"
-          >
-            <div className="absolute left-16 top-full -mt-16 transform-gpu opacity-50 blur-3xl xl:left-1/2 xl:-ml-80">
-              <div
-                className="aspect-[1154/678] w-[72.125rem] bg-gradient-to-br from-[#FF80B5] to-[#9089FC]"
-                style={{
-                  clipPath:
-                    "polygon(100% 38.5%, 82.6% 100%, 60.2% 37.7%, 52.4% 32.1%, 47.5% 41.8%, 45.2% 65.6%, 27.5% 23.4%, 0.1% 35.3%, 17.9% 0%, 27.7% 23.4%, 76.2% 2.5%, 74.2% 56%, 100% 38.5%)",
-                }}
-              />
-            </div>
-            <div className="absolute inset-x-0 bottom-0 h-px bg-gray-900/5" />
-          </div>
-          <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
-            <div className="mx-auto flex max-w-2xl items-center justify-between gap-x-8 lg:mx-0 lg:max-w-none">
-              <div className="flex items-center gap-x-6">
-                <Image
-                  src="/images/dashboard/client.png"
-                  alt=""
-                  className="h-16 w-16 flex-none rounded-full ring-1 ring-gray-900/10"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                />
-                <h1>
-                  <div className="mt-1 text-base font-semibold leading-6 text-gray-900">
-                    Chelsea Hagon
-                  </div>
-                </h1>
-              </div>
-            </div>
-          </div>
-        </header>
-        <div className="mx-auto px-4 py-16 sm:px-6 lg:px-8">
-          <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div className="">
-              <h2 className="text-sm font-semibold leading-6 text-gray-900">
-                Messages
-              </h2>
-              <ul role="list" className="mt-6 space-y-6">
-                {activity.map((activityItem, activityItemIdx) => (
-                  <li key={activityItem.id} className="relative flex gap-x-4">
-                    <div
-                      className={classNames(
-                        activityItemIdx === activity.length - 1
-                          ? "h-6"
-                          : "-bottom-6",
-                        "absolute left-0 top-0 flex w-6 justify-center"
-                      )}
-                    >
-                      <div className="w-px bg-gray-200" />
-                    </div>
-                    {activityItem.type === "commented" ? (
-                      <>
-                        <Image
-                          src={activityItem.person.imageUrl}
-                          alt=""
-                          className="relative mt-3 h-6 w-6 flex-none rounded-full bg-gray-50"
-                          width={0}
-                          height={0}
-                          sizes="100vw"
-                        />
-                        <div className="flex-auto rounded-md p-3 ring-1 ring-inset ring-gray-200">
-                          <div className="flex justify-between gap-x-4">
-                            <div className="py-0.5 text-xs leading-5 text-gray-500">
-                              <span className="font-medium text-gray-900">
-                                {activityItem.person.name}
-                              </span>{" "}
-                              sent
-                            </div>
-                            <time
-                              dateTime={activityItem.dateTime}
-                              className="flex-none py-0.5 text-xs leading-5 text-gray-500"
-                            >
-                              {activityItem.date}
-                            </time>
-                          </div>
-                          <p className="text-sm leading-6 text-gray-500">
-                            {activityItem.comment}
-                          </p>
-                        </div>
-                      </>
-                    ) : (
-                      <>
-                        <div className="relative flex h-6 w-6 flex-none items-center justify-center bg-white">
-                          {activityItem.type === "paid" ? (
-                            <CheckCircleIcon
-                              className="h-6 w-6 text-indigo-600"
-                              aria-hidden="true"
-                            />
-                          ) : (
-                            <div className="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
-                          )}
-                        </div>
-                        <p className="flex-auto py-0.5 text-xs leading-5 text-gray-500">
-                          <span className="font-medium text-gray-900">
-                            {activityItem.person.name}
-                          </span>{" "}
-                          {activityItem.type} the invoice.
-                        </p>
-                        <time
-                          dateTime={activityItem.dateTime}
-                          className="flex-none py-0.5 text-xs leading-5 text-gray-500"
-                        >
-                          {activityItem.date}
-                        </time>
-                      </>
-                    )}
-                  </li>
-                ))}
-              </ul>
-
-              {/* New comment form */}
-              <div className="mt-6 flex gap-x-3">
-                <Image
-                  src="/images/dashboard/user.png"
-                  alt=""
-                  className="h-6 w-6 flex-none rounded-full bg-gray-50"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                />
-                <form action="#" className="relative flex-auto">
-                  <div className="overflow-hidden rounded-lg pb-12 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
-                    <label htmlFor="comment" className="sr-only">
-                      Add your comment
-                    </label>
-                    <textarea
-                      rows={2}
-                      name="comment"
-                      id="comment"
-                      className="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="Add your message..."
-                      defaultValue={""}
-                    />
-                  </div>
-
-                  <div className="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
-                    <button
-                      type="submit"
-                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                    >
-                      Send Message
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
+      <div className="bg-oxford-blue">
+        <div className="mx-[56px] mt-5 ">
+          <div className={`${rubik.className} text-white `}>
+            <div className="text-[48px] font-bold">Alexander Wittenhaus</div>
+            <div className="text-base">Member Since: 19 February 2023</div>
+            <div className="text-base">Customer Value: $12,674</div>
           </div>
         </div>
-      </main>
+      </div>
+
+
+      {/* Tabs */}
+
+      <div className="px-[56px] pt-5 bg-oxford-blue">
+        <div className="sm:hidden">
+          <label htmlFor="tabs" className="sr-only">
+            Select a tab
+          </label>
+          {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+          <select
+            id="tabs"
+            name="tabs"
+            className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            defaultValue={tabs.find((tab) => tab.current)?.name}
+          >
+            {tabs.map((tab) => (
+              <option key={tab.name}>{tab.name}</option>
+            ))}
+          </select>
+        </div>
+        <div className="hidden sm:block">
+          <div className="">
+            <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+              {tabs.map((tab) => (
+                <a
+                  key={tab.name}
+                  href={tab.href}
+                  className={classNames(
+                    tab.current
+                      ? 'border-indigo-500 text-indigo-600'
+                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                    'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
+                  )}
+                  aria-current={tab.current ? 'page' : undefined}
+                >
+                  {tab.name}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      {/* Tab content         */}
+      <div className="flex-1 flex flex-column bg-delft-blue justify-center">
+
+        <div className="flex flex-col bg-oxford-blue w-1/2 my-10">
+
+          {/* Header */}
+          <div className="h-[50px] border-b-2 border-white m-5 flex flex-row items-center">
+
+            <div className="flex-1 text-white font-bold text-2xl">SMS Chat</div>
+            <div className="text-white flex-1 text-right flex justify-end"><Image src="/images/icons/SearchGlass.png" alt={"Search"} width={24} height={24} /></div>
+
+          </div>
+
+
+          {/* Messages  */}
+          <div className="h-full">
+            <ul>
+              {messages.map((message) => (
+                <li key={message.id}>
+                  <div className="flex flex-col place-items-end mr-5 mb-5">
+                    <ChatMessage id={message.id} speaker={message.speaker} message={message.message} time={message.time} style={ChatStyle.Trainer} />
+                  </div>
+                </li>
+              ))}
+
+
+            </ul>
+
+          </div>
+
+
+
+          {/* Input field */}
+          <form action={sendWithUpdate} className="h-[80px] border-2 border-white m-5 flex flex-row flex-wrap shadow-md bg-black space-x-0">
+
+
+            <div className="flex-grow">
+              <input
+                type="text"
+                name="message"
+                className="w-full h-[75px] bg-black text-white focus:outline-none hover:outline-none border-0"
+              />
+            </div>
+            <div className="flex-none flex flex-row justify-center items-center p-2">
+              <button type="submit" className="w-[48px] h-[48px] bg-button-1-bg flex flex-col justify-center items-center">
+                <Image
+                  src="/images/icons/right-chev.png"
+                  alt="Send message"
+                  width={18}
+                  height={18.02}
+                />
+
+              </button>
+            </div>
+
+            <div className="w-[30px]">
+              <input
+                type="text"
+                name="to"
+                placeholder="+1123456789"
+                className="bg-black  focus:outline-none text-white"
+              />
+            </div>
+
+          </form>
+
+        </div>
+
+      </div>
+
+
+
+
+
     </>
   );
+
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,11 @@
 
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
+import { Twilio } from "twilio";
+
+
+//Oddly can't get these from the env
+const twilioClient: Twilio = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export async function authenticate(
     prevState: string | undefined,
@@ -21,3 +26,27 @@ export async function authenticate(
         throw error;
     }
 }
+
+
+export async function sendMessage(
+    formData: FormData
+
+) {
+
+    //Not safe values could be empty
+    const toNumber = String(formData.get('to'))
+    const message = String(formData.get('message'));
+
+
+    try {
+        const tmessage = await twilioClient.messages.create({ to: toNumber, from: process.env.TWILIO_PHONE_NUMBER, body: message });
+        console.log("SMS message sent", tmessage.sid);
+    } catch (error) {
+        console.error("Error sending SMS", error);
+        throw error;
+    }
+
+
+}
+
+
